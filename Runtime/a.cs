75722959576b68a447f1a86b4e44adad382dad53
Theo-Desbonnets <theo.desbonnets@gfi.fr>
2020-09-09 16:59:55 +0200

    init package

---
+++ Runtime/a.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace test.test
+{
+    public class a : MonoBehaviour
+    {
+        public static string hello = "hello";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("hello");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
